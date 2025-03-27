44d7a878d225bed5e8faaef2b377620ac3e4952b
Emerson916 <emersons.a296@gmail.com>
2025-03-27 13:00:49 -0300

    Stripes webhooks

--- src/components/shared/client-providers.tsx
+++ src/components/shared/client-providers.tsx
@@ -1,16 +1,36 @@
 "use client"
-import { ReactNode } from "react";
+
+import { ReactNode, Suspense, useEffect } from "react";
 import { ThemeProvider } from "./theme-provider";
-import { Toaster } from "../ui/sonner";
-import { QueryClientProvider } from "@tanstack/react-query"
+import { QueryClientProvider } from "@tanstack/react-query";
 import { useTanstackQuery } from "@/lib/tanstack-query";
+import { Toaster } from "../ui/sonner";
+import { useSearchParams } from "next/navigation";
+import { toast } from "sonner";
+
+const CreditsToast = () => {
+  const searchParams = useSearchParams();
+
+  const successCheckoutParam = searchParams.get("success");
+
+  useEffect(() => {
+    if (successCheckoutParam === "true") {
+      toast.success(
+        "Compra realizada com sucesso! Seus créditos foram adicionados à sua conta."
+      )
+    }
+  }, [successCheckoutParam]);
+
+  return null;
+}
 
 type ClientProvidersProps = {
   children: ReactNode;
 };
 
 export const ClientProviders = ({ children }: ClientProvidersProps) => {
-  const queryClient = useTanstackQuery()
+  const queryClient = useTanstackQuery();
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
@@ -19,6 +39,9 @@ export const ClientProviders = ({ children }: ClientProvidersProps) => {
         enableSystem
         disableTransitionOnChange
       >
+        <Suspense>
+          <CreditsToast />
+        </Suspense>
         {children}
         <Toaster />
       </ThemeProvider>
